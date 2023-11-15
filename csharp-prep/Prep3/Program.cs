fb76163cae48f47164d66cf4f3ca6b2e0f077667
cheerios-design <samdotmc@gmail.com>
2023-11-15 12:33:27 +0300

    Updated with Stretch Challenges. Refer to strechChallenges-Prep2 (LOG section) for changes.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,18 +4,30 @@ class Program
 {
     static void Main()
     {
+        do
+        {
+            PlayGuessMyNumberGame();
+            
+            Console.Write("Do you want to play again? (yes/no): ");
+        } while (Console.ReadLine().ToLower() == "yes");
+    }
+
+    static void PlayGuessMyNumberGame()
+    {
         // Generate a random magic number between 1 and 100
         Random random = new Random();
         int magicNumber = random.Next(1, 101);
 
         Console.WriteLine("Welcome to the Guess My Number game!");
 
-        int userGuess;  // Declare userGuess outside the loop
+        int userGuess;
+        int numberOfGuesses = 0;
 
         do
         {
             Console.Write("What is your guess? ");
             userGuess = Convert.ToInt32(Console.ReadLine());
+            numberOfGuesses++;
 
             if (userGuess < magicNumber)
             {
@@ -27,7 +39,7 @@ class Program
             }
             else
             {
-                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"You guessed it in {numberOfGuesses} guesses!");
             }
 
         } while (userGuess != magicNumber);
