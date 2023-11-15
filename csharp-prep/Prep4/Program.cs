e715b64ee257ec7df322bf8ae5e42c94ac366b66
cheerios-design <samdotmc@gmail.com>
2023-11-15 12:40:29 +0300

    In this program:
    The user is prompted to enter a list of numbers until they enter 0.
    The list of numbers is stored in a List<int>.
    The sum, average, and maximum number in the list are computed using LINQ methods (Sum(), Average(), and Max()).
    The results are displayed.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int userInput;
+        do
+        {
+            Console.Write("Enter number: ");
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            if (userInput != 0)
+            {
+                numbers.Add(userInput);
+            }
+
+        } while (userInput != 0);
+
+        // Compute the sum
+        int sum = numbers.Sum();
+
+        // Compute the average
+        double average = numbers.Average();
+
+        // Find the maximum
+        int maxNumber = numbers.Max();
+
+        // Display results
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {maxNumber}");
     }
 }
