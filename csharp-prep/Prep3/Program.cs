94f3a68ca77771656339ee31b871c14f4ee5b051
cheerios-design <samdotmc@gmail.com>
2023-11-15 12:30:43 +0300

    completed /wout stretch.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,34 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Generate a random magic number between 1 and 100
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        Console.WriteLine("Welcome to the Guess My Number game!");
+
+        int userGuess;  // Declare userGuess outside the loop
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            userGuess = Convert.ToInt32(Console.ReadLine());
+
+            if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        } while (userGuess != magicNumber);
     }
 }
