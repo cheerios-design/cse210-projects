a086f3a7689bd14cc6d86c7c787ae4c451c8de8c
cheerios-design <samdotmc@gmail.com>
2023-11-23 21:35:18 +0300

    created Program.cs

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,25 @@
+// Program.cs
+
 using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Create an instance for 1/1 (using the first constructor)
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine($"Fraction 1: {fraction1.GetFractionString()}, Decimal Value: {fraction1.GetDecimalValue()}");
+
+        // Create an instance for 5/1 (using the second constructor)
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine($"Fraction 2: {fraction2.GetFractionString()}, Decimal Value: {fraction2.GetDecimalValue()}");
+
+        // Create an instance for 3/4 (using the third constructor)
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine($"Fraction 3: {fraction3.GetFractionString()}, Decimal Value: {fraction3.GetDecimalValue()}");
+
+        // Create an instance for 1/3 (using the third constructor)
+        Fraction fraction4 = new Fraction(1, 3);
+        Console.WriteLine($"Fraction 4: {fraction4.GetFractionString()}, Decimal Value: {fraction4.GetDecimalValue()}");
     }
 }
